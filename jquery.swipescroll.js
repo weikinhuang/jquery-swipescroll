546fbeb5f1392b435786be334b5371154ac0af71
Wei Kin Huang <weikin.huang04@gmail.com>
2011-09-02 19:54:31 +0000

    one finger kinetic scrolling plugin for overflow containers (X/Y axis)

--- jquery.swipescroll.js
+++ jquery.swipescroll.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI SwipeScroll 0.1.0
+ * jQuery UI SwipeScroll 0.2.0
  *
  * Copyright (c) 2011 Wei Kin Huang (<a href="http://www.closedinterval.com">Closed Interval</a>)
  *
@@ -10,9 +10,15 @@
  * Depends:
  *	jquery.ui.widget.js
  */
-(function($, undefined) {
+(function($) {
 	// check if this is a ipad
-	var isiPad = !!navigator.platform.match(/ipad/i);
+	var momentumDamp = "momentumDamp", momentumTime = "momentumTime", durationThreshold = "durationThreshold";
+	// if is a ipad, use the larger values
+	if (!!navigator.platform.match(/ipad/i)) {
+		momentumDamp = "iPadMomentumDamp";
+		momentumTime = "iPadMomentumTime";
+		durationThreshold = "iPadDurationThreshold";
+	}
 
 	$.widget("ui.swipescroll", {
 		widgetEventPrefix : "swipescroll",
@@ -25,24 +31,29 @@
 			iPadMomentumTime : 1200,
 			iPadDurationThreshold : 500,
 			eventSupressionThreshold : 10,
+			distanceThreshold : 20,
+			minMomentumDistance : 40,
+			maxMomentumDistance : 2000,
+			scrollX : false,
+			scrollY : true,
 			touchTags : [ "select", "input", "textarea" ]
 		},
 		_create : function() {
 			var self = this;
 
-			// bind the touch events
+			// bind the touchstart event
 			this.element.bind("touchstart.swipescroll", function(e) {
 				self._touchStart(e);
 			});
 		},
-		_setOption : function(key, value) {
-			$.Widget.prototype._setOption.apply(this, arguments);
-		},
 		destroy : function() {
+			// just remove all the events attached
 			this.element.unbind(".swipescroll");
+			// and do whatever else it needs to do
 			$.Widget.prototype.destroy.apply(this);
 		},
 		_getTouches : function(e) {
+			// try to get the touch handles
 			if (e.originalEvent) {
 				if (e.originalEvent.touches && e.originalEvent.touches.length) {
 					return e.originalEvent.touches;
@@ -52,94 +63,116 @@
 			}
 			return e.touches;
 		},
-		_getPosition : function() {
-			return this.element[0].scrollTop;
+		_getPositionX : function() {
+			return this.element.scrollLeft();
 		},
-		_scrollBy : function(y) {
-			return this.element.scrollTop((-y) + this._getPosition());
+		_getPositionY : function() {
+			return this.element.scrollTop();
 		},
-		_scrollTo : function(destY, time) {
-			if (destY === this._getPosition()) {
-				return;
+		_scrollBy : function(x, y) {
+			if (this.options.scrollY) {
+				this.element.scrollTop(y + this._getPositionY());
 			}
-
-			this.element.animate({
-				scrollTop : Math.max(destY, 0)
-			}, time, "easeOutCubic");
+			if (this.options.scrollX) {
+				this.element.scrollLeft(x + this._getPositionX());
+			}
 		},
-
-		// Perform a momentum-based scroll using CSS
-		_momentumScroll : function(d, k, minDist, maxDist, t) {
-			var ad = Math.abs(d), dy = 0;
+		_scrollTo : function(destX, destY) {
+			var animate = false, opts = {};
+			if (this.options.scrollY && destY !== this._getPositionY()) {
+				opts.scrollTop = Math.max(destY, 0);
+				animate = true;
+			}
+			if (this.options.scrollX && destX !== this._getPositionX()) {
+				opts.scrollLeft = Math.max(destX, 0);
+				animate = true;
+			}
+			// make sure something changed
+			if (animate) {
+				// do a jquery ui easing animation to handle the bicubic easing
+				this.element.animate(opts, this.options[momentumTime], "easeOutCubic");
+			}
+		},
+		_calculateDistance : function(distance, momentumDamp, maxDist) {
+			var absoluteDistance = Math.abs(distance), delta = 0;
+			// Calculate the total distance
+			while (absoluteDistance > 0.1) {
+				absoluteDistance *= momentumDamp;
+				delta += absoluteDistance;
+			}
+			// Limit to within min and max distances
+			if (delta > maxDist) {
+				delta = maxDist;
+			}
+			return distance < 0 ? -delta : delta;
+		},
+		_momentumScroll : function(x, y) {
+			var o = this.options, dy = 0, dx = 0;
 
 			// Calculate the total distance
-			while (ad > 0.1) {
-				ad *= k;
-				dy += ad;
-			}
+			dx = this._calculateDistance(x, o[momentumDamp], o.maxMomentumDistance);
+			dy = this._calculateDistance(y, o[momentumDamp], o.maxMomentumDistance);
 
-			// Limit to within min and max distances
-			if (dy > maxDist) {
-				dy = maxDist;
-			}
-			if (dy > minDist) {
-				if (d < 0) {
-					dy = -dy;
-				}
-				dy += this._getPosition();
-
+			if (Math.abs(dy) > o.minMomentumDistance || Math.abs(dx) > o.minMomentumDistance) {
 				// Perform scroll
-				this._scrollTo(Math.round(dy), t);
+				this._scrollTo(Math.round(dx + this._getPositionX()), Math.round(dy + this._getPositionY()));
 			}
 		},
-
-		// Perform a touch start event
 		_touchStart : function(e) {
-			var self = this, touch = this._getTouches(e);
+			// get the object that has the touch position
+			var self = this, touches = this._getTouches(e), data = touches[0] || e;
+
 			// Allow certain HTML tags to receive touch events or if we have already stopped the event propagation
-			if (touch.length === 1 || $.inArray(e.target.tagName.toLowerCase(), this.options.touchTags) !== -1) {
+			if (touches.length !== 1 || $.inArray(e.target.tagName.toLowerCase(), this.options.touchTags) !== -1) {
 				return;
 			}
 
 			// stop all animation
 			this.element.stop();
 
-			this.movedY = 0;
-			this.startTime = (new Date()).getTime();
-			// current touch scroll position
-			this.startY = touch[0].pageY;
-			this.touchY = touch[0].pageY;
+			// keep track of where and when we started
+			this.track = {
+				time : (new Date()).getTime(),
+				start : [ data.pageX, data.pageY ],
+				stop : [ data.pageX, data.pageY ]
+			};
 
 			// only bind the events if we are scrolling
 			this.element.bind("touchmove.swipescroll", function(e) {
+				// execute the touchmove callback
 				self._touchMove(e);
-			}).one("touchend.swipescroll touchcancel.swipescroll", function(e) {
+			}).one("touchend.swipescroll touchcancel.swipescroll", function() {
+				// unbind the events when touch end to save memory
 				self.element.unbind("touchmove.swipescroll touchend.swipescroll touchcancel.swipescroll");
-				self._touchEnd(e);
+				// execute the touchend callback
+				self._touchEnd();
 			});
 		},
-
-		// Perform a touch move event
 		_touchMove : function(e) {
-			// Stop the default scrolling
+			// get the object that has the touch position
+			var touches = this._getTouches(e), data = touches[0] || e;
+
+			// only 1 finger scrolling
+			if (touches.length !== 1) {
+				return;
+			}
+
+			// stop the default scrolling
 			e.preventDefault();
 
-			var touch = this._getTouches(e)[0];
-
-			var dy = touch.pageY - this.touchY;
-			this.touchY = touch.pageY;
-			this.movedY = this.startY - touch.pageY;
-
 			// scroll by the delta
-			this._scrollBy(dy);
-		},
+			this._scrollBy(this.track.stop[0] - data.pageX, this.track.stop[1] - data.pageY);
 
-		// Perform a touch end event
-		_touchEnd : function(e) {
-			// if we have moved, and we are doing momentum scrolling, then do kinetic scrolling
-			if (((new Date()).getTime() - this.startTime) < this.options.durationThreshold && Math.abs(this.movedY) > 0 && this.options.momentum) {
-				// Free scroll with momentum
-				this._momentumScroll(this.movedY, isiPad ? this.options.iPadMomentumDamp : this.options.momentumDamp, 40, 2000, isiPad ? this.options.iPadMomentumTime : this.options.momentumTime);
+			// keep track of where we are
+			this.track.stop = [ data.pageX, data.pageY ];
+		},
+		_touchEnd : function() {
+			// calculate the deltas
+			var o = this.options, dx = this.track.start[0] - this.track.stop[0], dy = this.track.start[1] - this.track.stop[1];
+
+			// do kinetic scrolling
+			if (o.momentum && (Math.abs(dx) > o.distanceThreshold || Math.abs(dy) > o.distanceThreshold) && (new Date()).getTime() - this.track.time < o[durationThreshold]) {
+				this._momentumScroll(dx, dy);
 			}
 		}
 	});
